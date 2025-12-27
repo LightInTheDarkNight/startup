99448cd0c5ec3ffae3dafb24a77c776a8e3b203e
AJ Boyd <aj.boyd2015@gmail.com>
2025-12-26 23:25:59 -0600

    play jsx now matches old (one more form to migrate)

--- Startup/src/landing/landing.jsx
+++ Startup/src/landing/landing.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
 export function Landing() {
     return (
@@ -14,30 +16,29 @@ export function Landing() {
                 (I don't care about feedback yet)@delversdomain.click!
             </p>
 
-            <div className="row row-cols-1 row-cols-md-3 g-3">
-                <div className="col">
-                    <a href="play.html" className="btn btn-outline-dark card h-100" style={{backgroundImage:"url('/Sword.ico')"}}>
-                        <div className="card-body text-light text-darken-bg">
+            <Row xs='1' md='3'>
+                <Col>
+                    <Card as={ Link } to='/play' className='btn btn-outline-dark h-100' style={{backgroundImage:"url('/Sword.ico')"}}>
+                        <Card.Body className='text-light text-darken-bg'>
                             Play
-                        </div>
-                    </a>
-                </div>
-                <div className="col">
-                    <a href="play.html" className="btn btn-outline-dark card h-100" style={{backgroundImage:"url('/Bottle.ico')"}}>
-                        <div className="card-body text-light text-darken-bg">
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card as={ Link } to='/chat' className='btn btn-outline-dark h-100' style={{backgroundImage:"url('/Bottle.ico')"}}>
+                        <Card.Body className='text-light text-darken-bg'>
                             Chat
-                        </div>
-                    </a>
-                </div>
-                <div className="col">
-                    <a href="play.html" className="btn btn-outline-dark card h-100" style={{backgroundImage:"url('/Camera.ico')"}}>
-                        <div className="card-body text-light text-darken-bg">
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card as={ Link } to='/play' className='btn btn-outline-dark h-100' style={{backgroundImage:"url('/Camera.ico')"}}>
+                        <Card.Body className='text-light text-darken-bg'>
                             Watch
-                        </div>
-                    </a>
-                </div>
-              </div>
-            
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </main>
     );
 }
--- Startup/src/play/play.jsx
+++ Startup/src/play/play.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Button, Stack, Form, FloatingLabel } from "react-bootstrap";
+import { } from "react-router-dom";
+import './play.css';
 
 export function Play() {
     return (
@@ -11,15 +14,15 @@ export function Play() {
                             Score: <span id="score">0</span>
                         </li>
                         <li className="gamebar-item">
-                            <label for="HP">Health: </label>
+                            <label htmlFor="HP">Health: </label>
                             <progress id="HP" max="100" value="75"></progress><br/>
-                            <label for="MP">Magic: </label>
+                            <label htmlFor="MP">Magic: </label>
                             <progress id="MP" max="100" value="75"></progress>
                         </li>
                         <li className="gamebar-item" style={{backgroundColor:'transparent'}}>
                             <div className="btn-group dropstart">
                                 <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                  <img src="Assets/settings.svg"/>
+                                  <img src="/settings.svg"/>
                                 </button>
                                 <ul className="dropdown-menu">
                                   {/* Dropdown menu links */}
@@ -32,23 +35,20 @@ export function Play() {
                     </ul>
                 </div>
                 <div id="game-start-container" className="position-relative p-0 mt-0" style={{height:'100%', minHeight:'450px'}}>
-                    <img src="Assets/gateOption.jpeg" className="position-absolute p-0 m-0 object-fit-cover" style={{top:0, left:0, width:'100%', height:'100%', filter:'brightness(40%)'}}/>
-                    <div id="game-start-settings" className="position-relative p-3 m-0 d-flex flex-column justify-content-center align-items-center text-light" style={{top:0, left:0, width:'100%', height:'100%'}}>
-                        <form method="get" action="play.html" className="d-flex mb-1">
-                            <div className="form-floating">
-                                <input type="text" id="jCode" placeholder="Join Code" className="form-control" required/>
-                                <label for="jCode" className="text-dark">Join a Game:</label>
-                            </div>
-                            <button type="submit" className="btn btn-primary ">Go</button>
-                        </form>
+                    <Stack className='p-3 m-1 justify-content-center align-items-center text-light'>
+                        <Form className='d-flex mb-1 g-3'>
+                            <FloatingLabel label='Join a Game:' controlId='joinID'>
+                                <Form.Control type='text' placeholder='Join Code:' id='joinID' required/>
+                            </FloatingLabel>
+                            <Button type='submit' variant='primary'>Go</Button>
+                        </Form>
                         <p>Or</p>
-                        <form method="get" action="play.html" className="d-flex mb-1">
-                            <div className="form-floating">
-                                <input type="text" id="wCode" placeholder="Watch Code" className="form-control" required/>
-                                <label for="wCode" className="text-dark">Watch a Game:</label>
-                            </div>
-                            <button type="submit" className="btn btn-primary">Go</button>
-                        </form>
+                        <Form className='d-flex mb-1 g-3'>
+                            <FloatingLabel label='Watch a Game:' controlId='watchID'>
+                                <Form.Control type='text' placeholder='watchCode' required/>
+                            </FloatingLabel>
+                            <Button type='submit' variant='primary'>Go</Button>
+                        </Form>
                         <span>Or</span>
                         <p>Start a new game:</p>
                         <form method="get" className="text-center" action="play.html">
@@ -60,19 +60,22 @@ export function Play() {
                             <h4>Character</h4>
                             <div className="btn-group d-flex" role="group" aria-label="Basic radio toggle button group">
                                 
-                                <input id="char1" type="radio" className="btn-check" name="character" value="swordsman" autocomplete="off" checked/>
-                                <label className="btn btn-outline-primary" for="char1">Swordsman</label>
+                                <input id="char1" type="radio" className="btn-check" name="character" value="swordsman" autoComplete="off" checked/>
+                                <label className="btn btn-outline-primary" htmlFor="char1">Swordsman</label>
 
-                                <input id="char2" type="radio" className="btn-check" name="character" value="cloak" autocomplete="off"/>
-                                <label className="btn btn-outline-primary" for="char2">Cloak</label>
+                                <input id="char2" type="radio" className="btn-check" name="character" value="cloak" autoComplete="off"/>
+                                <label className="btn btn-outline-primary" htmlFor="char2">Cloak</label>
                               
-                                <input id="char3" type="radio" className="btn-check" name="character" value="mage" autocomplete="off"/>
-                                <label className="btn btn-outline-primary" for="char3">Mage</label>
+                                <input id="char3" type="radio" className="btn-check" name="character" value="mage" autoComplete="off"/>
+                                <label className="btn btn-outline-primary" htmlFor="char3">Mage</label>
                             </div>
                             <br/>
                             <button type="submit" className="btn btn-primary">Play!</button>
                         </form>
-                    </div>
+                    </Stack>
+                    {/* <div id="game-start-settings" className="position-relative p-3 m-0 d-flex flex-column justify-content-center align-items-center text-light" style={{top:0, left:0, width:'100%', height:'100%'}}>
+                        
+                    </div> */}
                     
                 </div>
             </main>
